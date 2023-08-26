9ee810ae3143690cce79801acdb059e8227e91fe
khalid <khalidcomilla58@gmail.com>
2023-08-26 13:48:27 +0600

    Logic updated. Now we can check balance in multiple banks. While loop has been removed.

--- Atm.py
+++ Atm.py
@@ -8,27 +8,28 @@ class Atm:
         self.menu()
 
     def menu(self):
-        user_input = input("""
-                        1. Enter 1 to create pin
-                        2. Enter 2 to deposit
-                        3. Enter 3 to withdraw
-                        4. Enter 4 to check balance
-                        5 . Enter 5 to exit
-        """)
-
-        if user_input == "1":
-            self.create_pin()
-            print("Your pin has been set successfully")
-        elif user_input == "2":
-            self.deposit()
-        elif user_input == "3":
-            self.withdraw()
-        elif user_input == "4":
-            self.check_balance()
-        elif user_input == "5":
-            print("Thanks for being with us.\nBye")
-        else:
-            print("Please Enter a valid input")
+        print("hello")
+        # user_input = input("""
+        #                 1. Enter 1 to create pin
+        #                 2. Enter 2 to deposit
+        #                 3. Enter 3 to withdraw
+        #                 4. Enter 4 to check balance
+        #                 5. Enter 5 to exit
+        # """)
+        #
+        # if user_input == "1":
+        #     self.create_pin()
+        #     print("Your pin has been set successfully")
+        # elif user_input == "2":
+        #     self.deposit()
+        # elif user_input == "3":
+        #     self.withdraw()
+        # elif user_input == "4":
+        #     self.check_balance()
+        # elif user_input == "5":
+        #     print("Thanks for being with us.\nBye")
+        # else:
+        #     print("Please Enter a valid input")
 
     def create_pin(self):
         self.pin = input("Enter you pin please: ")
@@ -55,10 +56,14 @@ class Atm:
         print(f"Your current balance is : {self.balance}")
 
 
-prime = Atm("Prime Bank")
-AsiaBank = Atm("Asia Bank")
-DutchBanglaBank = Atm("Dutch Bangla Bank")
 
+prime = Atm("Prime Bank")
+prime.deposit()
+asia = Atm("Asia Bank")
+asia .deposit()
+
+print(prime.check_balance())
+print(asia.check_balance())
 # while 1:
 #     BankName = int(input("""Enter choose your Bank Name please:
 #                         1. Prime Bank
